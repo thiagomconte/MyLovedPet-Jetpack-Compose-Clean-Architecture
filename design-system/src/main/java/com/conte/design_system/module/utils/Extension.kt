5f3872fc742d24c03015a904c691d048e6666b6b
Thiago Marques Conte <thiagomconte@hotmail.com>
2024-02-12 00:44:13 -0300

    Refactor function

--- design-system/src/main/java/com/conte/design_system/module/utils/Extension.kt
+++ design-system/src/main/java/com/conte/design_system/module/utils/Extension.kt
@@ -1,12 +1,10 @@
 package com.conte.design_system.module.utils
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-@Composable
 fun Modifier.applyModifier(
     condition: Boolean,
-    block: @Composable Modifier.() -> Modifier
+    block: Modifier.() -> Modifier
 ): Modifier {
     return if (condition) {
         block()
